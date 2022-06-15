ba0e1220ab324c8990f421f96d4b7c15a95c956d
JIWON CHOI <rizjw26@gmail.com>
2022-06-15 18:51:52 +0900

    20220615_main1페이지 swipe slide 구현

---
+++ js/swiperHeaderSlide.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper',{
+    //optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesBetween: 0,
+    debugger: true,
+    loop: true,
+    centeredSlides: true,
+    speed: 1000,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+
+    // Pagination
+    pagination:{
+        el: '.swiper-pagination',
+        clickable : true,
+    },
+});
